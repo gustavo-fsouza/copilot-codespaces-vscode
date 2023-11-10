5a12e7332d9ff05f7d9e10a4b47dcf06ad229207
gustavo-fsouza <96851375+gustavo-fsouza@users.noreply.github.com>
2023-11-10 20:47:00 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    var memberBtn2 = document.getElementById("memberBtn2");
+    var skillsBtn2 = document.getElementById("skillsBtn2");
+
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberBtn.style.backgroundColor = "#f1f1f1";
+    skillsBtn.style.backgroundColor = "#e7e7e7";
+    memberBtn2.style.backgroundColor = "#f1f1f1";
+    skillsBtn2.style.backgroundColor = "#e7e7e7";
+}
